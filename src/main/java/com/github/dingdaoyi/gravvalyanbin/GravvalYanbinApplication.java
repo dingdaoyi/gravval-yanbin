d1cd71a81f4902c3bf76b9c56831d71ec8a895b2
岩冰 <1493001032@qq.com>
2024-09-20 17:00:12 +0800

    代码提交

---
+++ src/main/java/com/github/dingdaoyi/gravvalyanbin/GravvalYanbinApplication.java
@@ -0,0 +1,13 @@
+package com.github.dingdaoyi.gravvalyanbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravvalYanbinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravvalYanbinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/dingdaoyi/gravvalyanbin/GravvalYanbinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dingdaoyi.gravvalyanbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravvalYanbinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
